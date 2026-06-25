80b7c51c44bbd353b5585e9cd8fb6769f0a40ce6
Yoan Thirion <yoan.thirion@gmail.com>
2026-06-25 08:00:28 +0200

    feat: add url to test

--- tests/accessibility.spec.ts
+++ tests/accessibility.spec.ts
@@ -1,14 +1,20 @@
-import { test, expect } from '@playwright/test';
+import {test, expect, type Page} from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
+
+async function validateAccessibilityFor(page: Page, url: string) {
+    await page.goto(url);
+
+    const accessibilityScanResults = await new AxeBuilder({page})
+        .withTags(['RGAAv4', 'wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+        .analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+}
 
 test.describe('Pokedex home', () => {
     test('should not have any automatically detectable accessibility issues', async ({ page }) => {
-        await page.goto('/');
-
-        const accessibilityScanResults = await new AxeBuilder({ page })
-            .withTags(['RGAAv4', 'wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
-            .analyze();
-
-        expect(accessibilityScanResults.violations).toEqual([]);
+        for (const url of ['/', '/pokemon/2']) {
+            await validateAccessibilityFor(page, url);
+        }
     });
 });
