e811afa885167f2997bb795689e3bf13e2d69b94
Yoan Thirion <yoan.thirion@gmail.com>
2026-07-08 10:11:52 +0200

    fix: playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,27 +1,27 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-    testDir: './tests',
-    fullyParallel: true,
-    forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 1 : undefined,
-    reporter: 'html',
-    use: {
-        baseURL: 'http://localhost:5173/',
-        trace: 'on-first-retry',
-    },
+	testDir: "./tests",
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 2 : 0,
+	workers: process.env.CI ? 1 : undefined,
+	reporter: "html",
+	use: {
+		baseURL: "http://localhost:5173/",
+		trace: "on-first-retry",
+	},
 
-    projects: [
-        {
-            name: 'chromium',
-            use: { ...devices['Desktop Chrome'] },
-        }
-    ],
+	projects: [
+		{
+			name: "chromium",
+			use: { ...devices["Desktop Chrome"] },
+		},
+	],
 
-    webServer: {
-        command: 'npm run dev',
-        url: 'http://localhost:5173/',
-        reuseExistingServer: !process.env.CI
-    }
+	webServer: {
+		command: "npm run dev",
+		url: "http://localhost:5173/",
+		reuseExistingServer: !process.env.CI,
+	},
 });
